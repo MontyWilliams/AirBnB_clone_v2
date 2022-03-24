b545340751575f91738599210d02b7c52cfeb44e
Montywilliams <3650@holbertonschool.com>
2022-03-23 22:03:00 -0600

    Write a script that starts a Flask web application

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """tears down database"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """returns HTML page"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
